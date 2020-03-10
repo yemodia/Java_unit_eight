eb2dc5be96712557ceb56b223c744a85da7c1385
diaye <yerim-oumar.dia@ssfs.org>
2020-03-10 15:10:33 -0400

    Initial commit

---
+++ src/ThreeByFive.java
@@ -0,0 +1,59 @@
+
+    public class ThreeByFive {
+        public static void main(String [] args) {
+// note...this is a 2x5 int array
+// a 3x5 array is required
+            int[][] nums = { {3,6,2,9,7}, {7,45,9,12,44}, {4,98,34,45,21} };
+
+//this output is incomplete for this array
+
+            System.out.println(nums[0][0]);
+            System.out.println(nums[0][1]);
+            System.out.println(nums[0][2]);
+            System.out.println(nums[0][3]);
+            System.out.println(nums[0][4]);
+
+            System.out.println(nums[1][0]);
+            System.out.println(nums[1][1]);
+            System.out.println(nums[1][2]);
+            System.out.println(nums[1][3]);
+            System.out.println(nums[1][4]);
+
+            System.out.println(nums[2][0]);
+            System.out.println(nums[2][1]);
+            System.out.println(nums[2][2]);
+            System.out.println(nums[2][3]);
+            System.out.println(nums[2][4]);
+
+
+//change three other values to negative numbers
+            nums[1][4] = -4;
+            nums[0][0] = -6;
+            nums[2][1] = -27;
+            nums[2][2] = -16 ;
+
+//make a statement to create a blank line here
+
+// output all of the numbers again,
+// not just the changed ones
+
+            System.out.println(nums[0][0]);
+            System.out.println(nums[0][1]);
+            System.out.println(nums[0][2]);
+            System.out.println(nums[0][3]);
+            System.out.println(nums[0][4]);
+
+            System.out.println(nums[1][0]);
+            System.out.println(nums[1][1]);
+            System.out.println(nums[1][2]);
+            System.out.println(nums[1][3]);
+            System.out.println(nums[1][4]);
+
+            System.out.println(nums[2][0]);
+            System.out.println(nums[2][1]);
+            System.out.println(nums[2][2]);
+            System.out.println(nums[2][3]);
+            System.out.println(nums[2][4]);
+        }
+    }
+
---
+++ src/ThreeByThree.java
@@ -0,0 +1,44 @@
+public class ThreeByThree {
+    public static void main(String [] args){
+
+//code to define an initially empty double
+//2D array size 3x3
+
+        double [][] list = new double[3][3];
+
+//code to output all original values
+        System.out.println(list[0][0]);
+        System.out.println(list[0][1]);
+        System.out.println(list[0][2]);
+
+        System.out.println(list[1][0]);
+        System.out.println(list[1][1]);
+        System.out.println(list[1][2]);
+
+        System.out.println(list[2][0]);
+        System.out.println(list[2][1]);
+        System.out.println(list[2][2]);
+
+
+
+//code to change five of the values to positive
+        list[1][3] = 4;
+        list[0][0] = 6;
+        list[1][1] = 27;
+        list[2][2] = 16 ;
+
+//and the other four to negative
+        list[0][2] = -3;
+        list[2][0] = -78;
+        list[2][1] = -29;
+        list[1][2] = -6 ;
+
+
+//code to output a blank line separating output sets
+        
+
+
+//code to output all array elements
+    }
+
+}
